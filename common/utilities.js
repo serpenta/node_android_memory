efa72425119f2dd14f6b30084d70f1cde31b6486
Adrian Rutkiewicz <ignisserpenta@gmail.com>
2020-11-05 00:31:25 +0100

    add common utility functions

---
+++ common/utilities.js
@@ -0,0 +1,29 @@
+const getRadio = radioName =>
+{
+    const radio = document.getElementsByName(radioName);
+    let value = null;
+    radio.forEach(el => 
+        {
+            if (el.checked)
+            {
+                value = el.value;
+            } 
+        });
+    return value;
+}
+
+const getCheckbox = boxName =>
+{
+    return document.getElementById(boxName).checked;
+}
+
+const getInput = input =>
+{
+    return document.getElementById(input).value;
+}
+
+module.exports = {
+    getRadio,
+    getCheckbox,
+    getInput
+}
